0ba79a43aa2bc7bfa2755c6c3e540ba18115bc5a
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2023-12-01 22:45:26 -0500

    Delete old user edges display

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -386,26 +386,6 @@ function App() {
             focusedMethod={focusedMethod}
             setFocusedMethod={setFocusedMethod}
           />
-          {/* {userSelection.map((val, ind) => 
-            <div key={ind}>
-              {ind != 0 ? 
-                <Entity
-                  polyline={{
-                    show: true,
-                    width: 5,
-                    positions: 
-                      Cartesian3.fromDegreesArray(
-                        [curCities[val].lng, curCities[val].lat,
-                        curCities[userSelection[ind - 1]].lng, curCities[userSelection[ind - 1]].lat]
-                      ),
-                    material: userSelection.length > num ? Color.fromAlpha(Color.SKYBLUE, focusedMethod == 2 ? 1.0 : 0.5) : Color.fromAlpha(Color.LIGHTBLUE, focusedMethod == 2 ? 1.0 : 0.5),
-                    onClick: () => setFocusedMethod(2)
-                  }}
-                />
-              : <></>
-            }
-            </div>
-          )} */}
 
         </div>
         : <>Loading...</>}
