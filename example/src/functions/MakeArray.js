18c7137cce3f7c82a33134ab35c92a8ec5f0fcb3
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2023-12-03 20:02:40 -0500

    Remove unused functionality of makeArray to create 3d arrays

--- example/src/functions/MakeArray.js
+++ example/src/functions/MakeArray.js
@@ -4,12 +4,7 @@ export function makeArray(rows, cols, value) {
   for(let i = 0; i < rows; ++i) {
     let row = [];
     for(let j = 0; j < cols; ++j) {
-      if(value instanceof Array) {
-        row.push(value.slice());
-      }
-      else {
         row.push(value)
-      }
     }
     arr.push(row);
   }
